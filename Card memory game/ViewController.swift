d0a3c11e56d6800936cf4b8c1c0dfe1e15ba7954
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-05-10 21:01:57 +0300

    four cards with emoji

--- Card memory game/ViewController.swift
+++ Card memory game/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+
+    func flipButton (emoji: String, button: UIButton) {
+
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .systemMint
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        }
+    }
+
+let emojiCollection = ["🦊", "🐷", "🦊", "🐷"]
+
+
+
+    @IBOutlet var buttonCollection: [UIButton]!
+
     @IBOutlet weak var touchLabel: UILabel!
 
     @IBAction func buttonAction(_ sender: UIButton) {
+
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+        flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
     }
 
 }
